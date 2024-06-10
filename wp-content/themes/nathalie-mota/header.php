e583caa290d7dfca4c4d63c79de65508a2ea873f
Yazid Mare <yazid.mare.work@gmail.com>
2024-06-10 13:57:16 +0200

    Logo

--- wp-content/themes/nathalie-mota/header.php
+++ wp-content/themes/nathalie-mota/header.php
@@ -7,8 +7,18 @@
 </head>
 <body <?php body_class(); ?>>
 <header>
+    <div class="site-logo">
+        <a href="<?php echo esc_url(home_url('/')); ?>">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/Logo.png" alt="<?php bloginfo('name'); ?>">
+        </a>
+    </div>
     <h1><?php bloginfo('name'); ?></h1>
     <nav>
-        <?php wp_nav_menu(array('theme_location' => 'primary')); ?>
+        <?php
+        wp_nav_menu(array(
+            'theme_location' => 'primary',
+            'menu_class' => 'nav-menu',
+        ));
+        ?>
     </nav>
 </header>
