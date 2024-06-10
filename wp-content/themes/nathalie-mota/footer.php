fe764b5425b4148eb083c52fb384d4ed3323395b
Yazid Mare <yazid.mare.work@gmail.com>
2024-06-10 13:06:11 +0200

    Ajout du thème personnalisé Nathalie Mota

---
+++ wp-content/themes/nathalie-mota/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <p>&copy; <?php echo date('Y'); ?> - <?php bloginfo('name'); ?></p>
+    <button id="contactModalButton">Contact Us</button>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
